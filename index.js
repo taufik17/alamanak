aca63b7c119b3c4ba709eddf4f14aa08d5f3b149
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-11 21:15:20 +0700

    [undo] conf port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const xss = require('xss-clean');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || process.env.EXPRESS_PORT;
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const path = require('path');
